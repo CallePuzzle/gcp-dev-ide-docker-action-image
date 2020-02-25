a7af8d551230568c571a44ba837a6b554ae2e473
César M. Cristóbal <cesar@callepuzzle.com>
2020-02-25 09:52:02 +0100

    test users

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 set -e
 
 source /app/venv/bin/activate
-
+whoami
+id
 echo $INPUT_SA_KEY | base64 -di > service_account.json
 envsubst < inventory.gcp.tpl.yml | tee inventory.gcp.yml
 gcloud auth activate-service-account --key-file=service_account.json
