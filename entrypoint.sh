3fb6030a5e70352e17ddb65c51a56b4f52bb9ac6
César M. Cristóbal <cesar@callepuzzle.com>
2020-02-25 15:38:51 +0100

    Copy ansible.cfg to workdir

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ source /app/venv/bin/activate
 echo $INPUT_SA_KEY | base64 -di > service_account.json
 envsubst < /app/inventory.gcp.tpl.yml | tee inventory.gcp.yml
 gcloud auth activate-service-account --key-file=service_account.json
+cp /app/ansible.cfg ansible.cfg
 ansible-playbook -i inventory.gcp.yml /app/playbook.yaml
