147f1b7d6db85383fbcc107f98b5d255432a7872
César M. Cristóbal <cesar@callepuzzle.com>
2020-02-25 15:31:08 +0100

    Fix inventory path

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ source /app/venv/bin/activate
 echo $INPUT_SA_KEY | base64 -di > service_account.json
 envsubst < /app/inventory.gcp.tpl.yml | tee inventory.gcp.yml
 gcloud auth activate-service-account --key-file=service_account.json
-ansible-playbook -i /app/inventory.gcp.yml /app/playbook.yaml
+ansible-playbook -i inventory.gcp.yml /app/playbook.yaml
