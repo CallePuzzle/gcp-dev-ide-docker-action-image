acb43c6f5e6e9a36d4ddf75fa579f63af8fd867b
César M. Cristóbal <cesar@callepuzzle.com>
2020-02-27 00:36:07 +0100

    using instance name variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+INSTANCE_NAME=$(echo "$GITHUB_REPOSITORY-$INPUT_GITHUB_ACTOR_EMAIL" | perl -ne 'print lc' | perl -pe 's/.*\/(.*)/$1/s' | perl -pe 's/((?![a-z0-9]).)/-/gm')
+export INSTANCE_NAME
+
 source /app/venv/bin/activate
 echo $INPUT_SA_KEY | base64 -di > service_account.json
 envsubst < /app/inventory.gcp.tpl.yml | tee inventory.gcp.yml
+
 gcloud auth activate-service-account --key-file=service_account.json
+
 cp /app/ansible.cfg ansible.cfg
 ansible-playbook -i inventory.gcp.yml /app/playbook.yaml
